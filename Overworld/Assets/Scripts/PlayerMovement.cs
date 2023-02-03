842ea1fdaadf25a4f1824137f6bc1b2a9e8b3315
PolarsVisuals <119044199+PolarsVisuals@users.noreply.github.com>
2023-02-03 16:30:45 +0000

    Revert "Character and stuff"
    This reverts commit 398bff2b810b204154a7caa12b0905683601304a.

--- Overworld/Assets/Scripts/PlayerMovement.cs
+++ Overworld/Assets/Scripts/PlayerMovement.cs
@@ -4,43 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [Header("Player")]
-    public Transform orientation;
-
-    private float horizontalInput;
-    private float verticalInput;
-    private Vector3 moveDir;
-    private Rigidbody rb;
-
     [Header("Movement")]
-    public float walkSpeed;
-    public float sprintSpeed;
+    public float moveSpeed;
+
     public float groundDrag;
-    public bool isSprinting;
-
-    private float currentSpeed;
-
-    [Header("Jumping")]
-    public float jumpForce;
-    public float jumpCooldown;
-    public float airMultiplayer;
-
-    private bool readyToJump;
-
-    [Header("Keybinds")]
-    public KeyCode jumpKey = KeyCode.Space;
 
     [Header("Ground Check")]
     public float playerHeight;
     public LayerMask whatIsGround;
     private bool isGrounded;
 
+    public Transform orientation;
+
+    private float horizontalInput;
+    private float verticalInput;
+
+    private Vector3 moveDir;
+
+    private Rigidbody rb;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         rb.freezeRotation = true;
-
-        readyToJump = true;
     }
 
     private void Update()
@@ -71,26 +57,6 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
-
-        //Jump
-        if(Input.GetKey(jumpKey) && readyToJump && isGrounded)
-        {
-            readyToJump = false;
-
-            Jump();
-
-            Invoke(nameof(ResetJump), jumpCooldown);
-        }
-
-        //Sprint
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
-            isSprinting = true;
-        }
-        else
-        {
-            isSprinting = false;
-        }
     }
 
     private void MovePlayer()
@@ -98,48 +64,18 @@ public class PlayerMovement : MonoBehaviour
         //Calculate movement direction
         moveDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
-        if (isGrounded)
-        {
-            rb.AddForce(moveDir.normalized * currentSpeed * 10f, ForceMode.Force);
-        }
-        else
-        {
-            rb.AddForce(moveDir.normalized * currentSpeed * 10f * airMultiplayer, ForceMode.Force);
-        }
-       
+        rb.AddForce(moveDir.normalized * moveSpeed * 10f, ForceMode.Force);
     }
 
     private void SpeedControl()
     {
         Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
-        if (isSprinting)
-        {
-            currentSpeed = sprintSpeed;
-        }
-        else
-        {
-            currentSpeed = walkSpeed;
-        }
-
         //Limit velocity if needed
-        if(flatVel.magnitude > walkSpeed)
+        if(flatVel.magnitude > moveSpeed)
         {
-            Vector3 limitedVel = flatVel.normalized * walkSpeed;
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
             rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
         }
-    }
-
-    private void Jump()
-    {
-        //Reset Y velocity
-        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-
-        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
-    }
-
-    private void ResetJump()
-    {
-        readyToJump = true;
     }
 }
