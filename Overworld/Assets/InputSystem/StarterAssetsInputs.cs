44494f0463320dd1dc8e8239064665c08e220d8f
PolarsVisuals <119044199+PolarsVisuals@users.noreply.github.com>
2023-02-06 11:25:36 +0000

    Attacking Input

--- Overworld/Assets/InputSystem/StarterAssetsInputs.cs
+++ Overworld/Assets/InputSystem/StarterAssetsInputs.cs
@@ -13,6 +13,7 @@ namespace StarterAssets
 		public bool jump;
 		public bool sprint;
 		public bool draw;
+		public bool shoot;
 
 		[Header("Movement Settings")]
 		public bool analogMovement;
@@ -49,6 +50,11 @@ namespace StarterAssets
 		{
 			DrawInput(value.isPressed);
 		}
+
+		public void OnShoot(InputValue value)
+		{
+			ShootInput(value.isPressed);
+		}
 #endif
 
 
@@ -75,6 +81,11 @@ namespace StarterAssets
 		public void DrawInput(bool newDrawState)
 		{
 			draw = newDrawState;
+		}
+
+		public void ShootInput(bool newShootState)
+		{
+			shoot = newShootState;
 		}
 
 		private void OnApplicationFocus(bool hasFocus)
--- Overworld/Assets/Scripts/PlayerCombat.cs
+++ Overworld/Assets/Scripts/PlayerCombat.cs
@@ -5,6 +5,10 @@ using StarterAssets;
 
 public class PlayerCombat : MonoBehaviour
 {
+    float timePassed;
+    float clipLength;
+    float clipSpeed;
+
     [SerializeField] GameObject weapon;
     [SerializeField] GameObject weaponHolder;
     [SerializeField] GameObject sheathHolder;
@@ -16,12 +20,14 @@ public class PlayerCombat : MonoBehaviour
     private StarterAssetsInputs starterAssetsInputs;
 
     private bool weaponDrawn;
+    private bool isAttacking;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
         starterAssetsInputs = GetComponent<StarterAssetsInputs>();
         weaponDrawn = false;
+        isAttacking = false;
     }
 
     private void Start()
@@ -33,7 +39,7 @@ public class PlayerCombat : MonoBehaviour
     {
         if (starterAssetsInputs.draw)
         {
-            Debug.Log("Running");
+            //Debug.Log("Running");
             if (!weaponDrawn)
             {
                 animator.SetTrigger("drawWeapon");
@@ -47,6 +53,44 @@ public class PlayerCombat : MonoBehaviour
 
             starterAssetsInputs.draw = false;
         }
+
+        if (starterAssetsInputs.shoot)
+        {
+            GetComponent<ThirdPersonController>().canMove = false;
+
+            InitiateAttack();
+
+            starterAssetsInputs.shoot = false;
+        }
+
+        if(isAttacking)
+        {
+            timePassed += Time.deltaTime;
+            clipLength = animator.GetCurrentAnimatorClipInfo(1)[0].clip.length;
+            clipSpeed = animator.GetCurrentAnimatorStateInfo(1).speed;
+
+            if(timePassed >= clipLength / clipSpeed && starterAssetsInputs.shoot)
+            {
+                animator.SetTrigger("attack");
+            }
+            if(timePassed >= clipLength / clipSpeed)
+            {
+                animator.applyRootMotion = false;
+                animator.SetTrigger("canMove");
+                GetComponent<ThirdPersonController>().canMove = true;
+                isAttacking = false;
+            }
+        }
+    }
+
+    public void InitiateAttack()
+    {
+        animator.applyRootMotion = true;
+        timePassed = 0f;
+        animator.SetTrigger("attack");
+        animator.SetFloat("Speed", 0f);
+
+        isAttacking = true;
     }
 
     public void DrawWeapon()
@@ -59,5 +103,10 @@ public class PlayerCombat : MonoBehaviour
     {
         currentWeaponInSheath = Instantiate(weapon, sheathHolder.transform);
         Destroy(currentWeaponInHand);
+    }
+
+    public void NewEvent()
+    {
+        Debug.Log("Delete blank event.");
     }
 }
